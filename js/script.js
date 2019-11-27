ae9940bfc3ef96533bcf4e87599675f76e612a95
Product0wner <jaanbrunken@gmail.com>
2019-11-27 16:26:43 +0100

    all functionality build

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
 
 //const listItemsShown;
 const studentList = document.querySelector("UL");
-const itemsOfStudenList = studentList.children;
+const students = studentList.children;
 const paginationList = document.createElement("UL");
+const searchButton = document.querySelector("div.student-search button");
 
 // only show the 10 first elements
-for(let i = 10; i < itemsOfStudenList.length; i++){
-   itemsOfStudenList[i].style.display = "none";
+for(let i = 10; i < students.length; i++){
+   students[i].style.display = "none";
 }
 
-// First step create paginationList and append to the DOM
+// Search Feature
+searchButton.addEventListener("click", (e) => {
+   let search = document.querySelector("div.student-search input");
+   for (let i = 0; i < students.length; i++) {
+      let studentName = students[i].querySelector("h3").textContent;
+      if (studentName.includes(search.value)){
+         students[i].style.display = "block";
+         console.log(studentName)
+      } else {
+         students[i].style.display = "none";
+      }
+   }; 
+   search.value = "";
+});
+
+// Create paginationList and append to the DOM
 let createPagination = () => {
    paginationList.className = "pagination";
    studentList.parentNode.insertBefore(paginationList, studentList.nextSibling);
 
    // Then create 1 new li item for every 10 ListItems and append them to the paginationList
-   for(let i = 0; i < itemsOfStudenList.length; i++) {
+   for(let i = 0; i < students.length; i++) {
       if(i % 10 == 0) {
          let newLi = document.createElement("li");
          let pageIndex = paginationList.childNodes.length + 1;
@@ -33,6 +49,7 @@ let createPagination = () => {
 }
 
 
+// Navigate through pagination
 paginationList.addEventListener("click", (e) => {
    e.preventDefault();
    targetIndex = parseInt(e.target.textContent, 10);
@@ -45,67 +62,14 @@ paginationList.addEventListener("click", (e) => {
    e.target.className ="active";
 
    // Hide all items of the student list
-   for(let i = 0; i < itemsOfStudenList.length; i++){
-      itemsOfStudenList[i].style.display = "none";
+   for(let i = 0; i < students.length; i++){
+      students[i].style.display = "none";
    }
    
    // Show the Students for clicked pagination - Every page should have maximum of 10 Students.
    for(let i = (targetIndex-1)*10; i < targetIndex*10 ; i++){
-   itemsOfStudenList[i].style.display = "block";
+   students[i].style.display = "block";
    }
 });
 
-
-
 createPagination();
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-/*
-if tragetIndex is 1 dann Starte Bei (TargetIndex-1*10) Ende bei TargetIndex*10
-if tragetIndex is 2 dann zeige (TargetIndex-1*10) bis TargetIndex*10
-
-*/
-
-/*
-take list of all students and loop through.
-if (click on element 1) display list items with i smaller than 10
-if (click in element 2) display list items with i <10 and smaller 20 smaller
-if (click on elemt 3)
-
-
-
-*/
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-
-   interate trough the length of the student list
-   after every 10th items in the list create a new page.
-
-***/
-
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
