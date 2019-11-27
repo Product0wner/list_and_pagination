fd5bac6bad8ed514fa078b912368af9f4eb710b1
Product0wner <jaanbrunken@gmail.com>
2019-11-27 20:20:56 +0100

    final changes & docu

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 
-//const listItemsShown;
-const studentList = document.querySelector("UL");
-const students = studentList.children;
-const paginationList = document.createElement("UL");
+const students = document.querySelectorAll("UL.student-list > li ");
+const studentsPerPage = 10;
+const pagination = document.createElement("UL");
 const searchButton = document.querySelector("div.student-search button");
 
-// only show the 10 first elements
-for(let i = 10; i < students.length; i++){
+// Function to only show the stundents for selected pagination
+let showStudents = (list, page) => {
+   let startIndex = (page -1) * studentsPerPage;
+   let stopIndex = (page * studentsPerPage)-1;
+   for(let i = 0; i < list.length; i++){
+      if(i < startIndex || i > stopIndex) {
+         list[i].style.display = "none";
+      } else {
+         list[i].style.display = "block";
+      }
+   }
+}
+
+
+// Show the first 10 stundes when page is loaded
+for(let i = studentsPerPage; i < students.length; i++){
    students[i].style.display = "none";
 }
 
-// Search Feature
+// Search for students by name
 searchButton.addEventListener("click", (e) => {
    let search = document.querySelector("div.student-search input");
    for (let i = 0; i < students.length; i++) {
       let studentName = students[i].querySelector("h3").textContent;
       if (studentName.includes(search.value)){
          students[i].style.display = "block";
-         console.log(studentName)
       } else {
          students[i].style.display = "none";
       }
@@ -25,51 +37,48 @@ searchButton.addEventListener("click", (e) => {
    search.value = "";
 });
 
-// Create paginationList and append to the DOM
-let createPagination = () => {
-   paginationList.className = "pagination";
-   studentList.parentNode.insertBefore(paginationList, studentList.nextSibling);
 
-   // Then create 1 new li item for every 10 ListItems and append them to the paginationList
+
+// Create pagination and append to the DOM
+let createPaginationPages = () => {
+   pagination.className = "pagination";
+   let studentList = students[0].parentNode;
+   studentList.parentNode.insertBefore(pagination, studentList.nextSibling);
+
+   // Then create 1 new li item for every 10 ListItems and append them to the pagination
    for(let i = 0; i < students.length; i++) {
-      if(i % 10 == 0) {
+      if(i % studentsPerPage === 0) {
          let newLi = document.createElement("li");
-         let pageIndex = paginationList.childNodes.length + 1;
+         let pageIndex = pagination.childNodes.length + 1;
 
          // Make the first item by default a different class compared to following items
          if(pageIndex === 1) {
             newLi.innerHTML = "<a class='active' href=#>"+ pageIndex +"</a>";
-            paginationList.appendChild(newLi);
+            pagination.appendChild(newLi);
          } else {
             newLi.innerHTML = "<a class='' href=#>"+ pageIndex +"</a>";
-            paginationList.appendChild(newLi);
+            pagination.appendChild(newLi);
          } 
       }
    }
 }
+createPaginationPages();
 
-
-// Navigate through pagination
-paginationList.addEventListener("click", (e) => {
+// Update based on click on pagination
+pagination.addEventListener("click", (e) => {
    e.preventDefault();
-   targetIndex = parseInt(e.target.textContent, 10);
 
-   // Make all items not selected
-   for(i=0; i < paginationList.children.length; i++){
-      paginationList.childNodes[i].lastChild.className = '';
+   // Make all items not selected again
+   for(i=0; i < pagination.children.length; i++){
+      pagination.childNodes[i].lastChild.className = '';
    }
    // Make the clicked item selected
    e.target.className ="active";
+   let pageIndex = parseInt(e.target.textContent, 10);
 
-   // Hide all items of the student list
-   for(let i = 0; i < students.length; i++){
-      students[i].style.display = "none";
-   }
-   
-   // Show the Students for clicked pagination - Every page should have maximum of 10 Students.
-   for(let i = (targetIndex-1)*10; i < targetIndex*10 ; i++){
-   students[i].style.display = "block";
-   }
+   // show only the students for selected page.
+   showStudents(students,pageIndex);
 });
 
-createPagination();
+
+
